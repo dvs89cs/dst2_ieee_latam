2dd757dd3c1d4eb12cb7006e121ed7b417815e36
dvs89cs <36409436+dvs89cs@users.noreply.github.com>
2024-01-03 11:27:54 -0500

    Update dst2_code.py

--- dst2_code IEEE-LATAM.py
+++ dst2_code IEEE-LATAM.py
@@ -1,7 +1,6 @@
 ###############################################################################
-# El código corresponde al artículo "Iterative Numerical Method to Construct 
-# Wavelets Matching Unidimensional Patterns" sometido a IEEE Latin American 
-# Transactions (2022).
+# El código corresponde al artículo "A new method for 2-D adapted wavelet construction: 
+# an application on mass type anomalies localization in mammographic images" sometido a MDPI (2023).
 # Autor: Damian Valdés Santiago© (dvs89cs@gmail.com)
 # Licencia MIT.
 ###############################################################################
